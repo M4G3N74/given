c225e40757707d7e330136bf33618b90b182f828
Given Malambo <malambogiven6@gmail.com>
2022-09-19 20:53:11 -0700

    new updates for hero deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ot0^xja6$m=m)je+h&^@g(ln$=4ip_k!y9!84w=o41auuh#&y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
